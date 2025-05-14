518f9edd9b4992c42463a80daf7d6a0edd8220e6
monge <ojdupool2004@mail.ru>
2025-05-14 13:46:42 +0300

    тесты для userauth

--- backend/userauth/tests.py
+++ backend/userauth/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework import status
 
-# Create your tests here.
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='password123')
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_change_password(self):
+        url = reverse('userauth:change-password')
+        data = {'currentPassword': 'password123', 'newPassword': 'newpassword123'}
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, 'Пароль изменён')
+
